dbcbd3ef2bff5b4cc7c0f0623372fadcc4d86748
YohananDiamond <mitonanan12@gmail.com>
2020-09-03 22:49:08 -0300

    Adicionado check para versão (deve ser 3.x)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+import sys
+import platform
+
+major_version = platform.python_version()[0]
+if not (major_version == "3"):
+    print(f"Major python version needs to be 3 (current is {major_version}).", file=sys.stderr)
+    sys.exit(1)
+
 import core
 import game
-
-# TODO: python3 guard
 
 if __name__ == "__main__":
     game.main()
