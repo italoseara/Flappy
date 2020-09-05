5badc7ff9c09d49b4daab9e675086a048be183ca
YohananDiamond <mitonanan12@gmail.com>
2020-09-05 15:58:05 -0300

    Better preparation scripts

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
 	cd src && python3 main.py
+
+pip:
+	python3 -m pip install pygame
---
+++ make.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set ARG=%1
+set PYCMD=
+
+where /q py && set PYCMD=py
+where /q python && set PYCMD=python
+where /q python3 && set PYCMD=python
+where /q python3.7 && set PYCMD=python
+where /q python3.8 && set PYCMD=python
+
+if "%PYCMD%" == "" (
+   echo Could not find python binary in PATH.
+   exit /b
+)
+
+if "%1" == "pip" (
+   %PYCMD% -m pip install pygame
+) else if "%1" == "run" (
+   cd src
+   %PYCMD% main.py
+   cd ..
+) else (
+   echo Unknown subcommand: "%1"
+   goto usage
+)
+
+goto end
+
+:usage
+echo Usage: %0 { pip ^| run }
+exit /b
+
+:end
+@echo on
