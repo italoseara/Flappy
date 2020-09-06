c681904eb8101bc53c5be89f3c8167f6f7c97aba
YohananDiamond <mitonanan12@gmail.com>
2020-09-06 10:15:37 -0300

    Better version check

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+from __future__ import print_function
 import sys
 import platform
 import os
 
-# check if python version is 3.x (needed)
-major_version = platform.python_version()[0]
-if not (major_version == "3"):
-    print(f"Major python version needs to be 3 (current is {major_version}).", file=sys.stderr)
+# TODO: logger
+
+# check if python version is 3.7+ (needed)
+# still, this file should be python2-compliant for the check to run.
+major, minor = sys.version_info[:2]
+if sys.version_info < (3, 7):
+    print("You need to use at least python 3.7 (current is ~{}.{})".format(major, minor), file=sys.stderr)
     sys.exit(1)
 
 # import pygame and try to supress the annoying startup message.
