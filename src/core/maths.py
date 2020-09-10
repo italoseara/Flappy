0fdc0b5ed974f935b86224ec911bc8c3a5b0e9d1
YohananDiamond <mitonanan12@gmail.com>
2020-09-09 22:01:26 -0300

    Added a "Rectangle" class (currently not used)

--- src/core/maths.py
+++ src/core/maths.py
@@ -41,3 +41,22 @@ class Vector2:
 
     def into_tuple(self) -> tuple:
         return (self.x, self.y)
+
+class Rectangle:
+    def __init__(self, *args):
+        if len(args) == 4:
+            self.x, self.y, self.width, self.height = args
+        elif len(args) == 1 and isinstance(args[0], tuple) and len(args[0]) == 4:
+            self.x, self.y, self.width, self.height = args[0]
+        elif (len(args) == 2
+              and isinstance(args[0], tuple)
+              and len(args[0]) == 2
+              and isinstance(args[1], tuple)
+              and len(args[1]) == 2):
+            self.x, self.y = args[0]
+            self.width, self.height = args[1]
+        else:
+            raise TypeError("could not find matching pattern for argument list: {}{}".format(
+                type(self),
+                args,
+            ))
